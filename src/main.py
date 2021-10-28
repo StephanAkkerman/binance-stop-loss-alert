0498dd9bfd800a2b55aed6214d5292bc1711de89
Akkie100 <stephan.akkerman@gmail.com>
2021-10-28 14:40:41 +0200

    File structure changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import (
+    BinanceWebSocketApiManager,
+)
+from unicorn_binance_rest_api.unicorn_binance_rest_api_exceptions import (
+    BinanceAPIException,
+)
 import datetime
 import json
 import requests
@@ -19,6 +24,7 @@ send_to = binance_keys.send_to
 
 # This will contain the symbols that have been sent the last minute
 sent = []
+
 
 def sendSellAlert(bot_message):
     """ 
@@ -40,13 +46,15 @@ def sendSellAlert(bot_message):
     )
     response = requests.get(send_text)
 
+
 def clearSent():
     """ Removes symbol from sent list """
     sent.pop(0)
 
+
 def userInfo(info_string, stream_buffer_name=False):
     """ Getting user data, through ubwa_user, sends a message if an asset has been sold except for market orders """
-    
+
     try:
         # Convert info from string to dict
         info = json.loads(info_string)
@@ -72,16 +80,18 @@ def userInfo(info_string, stream_buffer_name=False):
                     if orderType != "MARKET":
                         if sym not in sent:
                             msgPrice = round(float(execPrice), 4)
-                            msg = " ".join([orderType, "sold", sym, "$" + str(msgPrice)])
+                            msg = " ".join(
+                                [orderType, "sold", sym, "$" + str(msgPrice)]
+                            )
                             sendSellAlert(msg)
                             sent.append(sym)
 
                             # Remove after 60 sec
                             threading.Timer(60.0, clearSent).start()
 
-    except Exception  as e:
-        print(e)
-        print("Error in userInfo")
+    except BinanceAPIException:
+        pass
+
 
 if __name__ == "__main__":
 
@@ -94,34 +104,39 @@ if __name__ == "__main__":
         spot_user.create_stream(
             "arr", "!userData", api_key=public_key, api_secret=private_key
         )
-    
+
         # Margin
-        margin_user = BinanceWebSocketApiManager(process_stream_data=userInfo, exchange="binance.com-margin")
+        margin_user = BinanceWebSocketApiManager(
+            process_stream_data=userInfo, exchange="binance.com-margin"
+        )
         margin_user.create_stream(
             "arr", "!userData", api_key=public_key, api_secret=private_key
         )
 
-        # Isolated Margin 
+        # Isolated Margin
         # ONLY WORKS FOR 1 SYMBOL
-        #iso_margin_user = BinanceWebSocketApiManager(process_stream_data=userInfo, exchange="binance.com-isolated_margin")
-        #iso_margin_user.create_stream(
+        # iso_margin_user = BinanceWebSocketApiManager(process_stream_data=userInfo, exchange="binance.com-isolated_margin")
+        # iso_margin_user.create_stream(
         #    "arr", "!userData", api_key=public_key, api_secret=private_key, symbol = "btcusdt"
-        #)
+        # )
 
         # USD-M Futures
-        usd_futures_user = BinanceWebSocketApiManager(process_stream_data=userInfo, exchange="binance.com-futures")
+        usd_futures_user = BinanceWebSocketApiManager(
+            process_stream_data=userInfo, exchange="binance.com-futures"
+        )
         usd_futures_user.create_stream(
             "arr", "!userData", api_key=public_key, api_secret=private_key
         )
 
         # Coin-M Futures
-        coin_futures_user = BinanceWebSocketApiManager(process_stream_data=userInfo, exchange="binance.com-coin-futures")
+        coin_futures_user = BinanceWebSocketApiManager(
+            process_stream_data=userInfo, exchange="binance.com-coin-futures"
+        )
         coin_futures_user.create_stream(
             "arr", "!userData", api_key=public_key, api_secret=private_key
         )
 
-    except Exception as e:
-        print(e)
-        print("Error with sockets")
+    except BinanceAPIException:
+        pass
 
 # Maybe restart every 24h https://oliver-zehentleitner.github.io/unicorn-binance-websocket-api/unicorn_binance_websocket_api.html?highlight=replace_stream#unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager.BinanceWebSocketApiManager.replace_stream
